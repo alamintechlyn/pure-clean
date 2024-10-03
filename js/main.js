59e932becb03f55ee8cb3da42dcf1cec30eac63f
alamin <alamintechlyn>
2024-10-03 17:52:11 +0600

    add product details page

--- js/main.js
+++ js/main.js
@@ -129,3 +129,27 @@ function accordianFunc(index) {
 }
 
 
+// ===============================================================
+                  // Product page js
+// ===================================================================
+
+
+
+// product details slider
+
+var swiper = new Swiper(".myProductSwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".myProductSwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
